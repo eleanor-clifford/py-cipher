dd964a15ce12d4e099589b0be28be4284956c9f4
Ellie Clifford <git@clifford.lol>
2017-09-12 17:42:24 +0100

    removed accidental cached files

--- affineCipher.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import core
-input1 = input()
-f = core.frequencyList(input1)
-sortedShiftList = core.sortLinear(lambda x, a, b: a*x + b, input1, range(1,26), range(26), f)
-print(core.dictionaryFilter(lambda x, a, b: a*x + b,input1,sortedShiftList))
--- recursiveDictTest.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-import core
-input1 = input()
-print(core.recursiveDictionaryCheck(input1,0,1))
